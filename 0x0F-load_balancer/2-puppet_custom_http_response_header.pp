d75fd72e037f05591fd95655df8663692cceeea8
juansu01 <thejuank0@hotmail.com>
2021-08-23 15:49:56 -0500

    Telling puppet to also install nginx

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,5 @@
 # This file configures a custom header
 exec {'install nginx':
-    command  => 'sudo sed -i "23i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf && sudo service nginx restart'
+    command  => 'sudo apt-get update -y && sudo apt-get install nginx -y && sudo sed -i "23i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf && sudo service nginx restart'
     
 }
