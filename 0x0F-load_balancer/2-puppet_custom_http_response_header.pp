b13f0ca5828ebd056a64589bb9b5f179b8a7f9ae
juansu01 <thejuank0@hotmail.com>
2021-08-23 15:52:37 -0500

    Adding provider

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,5 @@
 # This file configures a custom header
-exec {'install nginx':
+exec {'Installing nginx and configuring header':
     command  => 'sudo apt-get update -y && sudo apt-get install nginx -y && sudo sed -i "15i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf && sudo service nginx restart'
-    
+    provider => shell
 }
