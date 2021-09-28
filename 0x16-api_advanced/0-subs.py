e66dcc00099610a5cd583072afa377912c6f4e93
juansu01 <thejuank0@hotmail.com>
2021-09-28 05:55:16 -0500

    Adding first task

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module consumes the reddit api"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    my_header = {'User-agent': 'Mozilla/5.0'}
+    r = requests.get(
+        'https://www.reddit.com/r/{}/about.json'.format(
+            subreddit), headers=my_header)
+    if r.status_code != 200:
+        return 0
+    to_json = r.json()
+    my_data = to_json['data']['subscribers']
+    return my_data
+
+
+print(number_of_subscribers("programming"))
