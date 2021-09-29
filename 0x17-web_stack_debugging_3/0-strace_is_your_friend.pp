af0c834c1c865433b5b330acb775a5ec2ebf4ba4
juansu01 <thejuank0@hotmail.com>
2021-09-29 09:35:31 -0500

    Fixing syntax errors and adding path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 # Fix typo! there's an extra p                             
-exec { 'remove extra p':                                   
-command => 'sed -i 's/phpp/php/' /var/www/html/wp-settings.php'
-}   
+exec { 'remove extra p':
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php"
+}
