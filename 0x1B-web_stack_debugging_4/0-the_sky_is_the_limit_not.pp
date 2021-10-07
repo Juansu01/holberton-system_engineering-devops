40cb6f65e16fabc65a095652488681e010058d54
Juan Camilo <70981507+Juansu01@users.noreply.github.com>
2021-10-07 13:14:25 -0500

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,11 +5,6 @@ exec { "change workers":
 }
 # Restart the nginx service
 exec { "restart service":
-        command => "sed -i 's/# multi_accept on;/multi_accept on;/g' /etc/nginx/nginx.conf",
-        provider => 'shell',
-}
-# Restart the nginx service
-exec { "restart service":
         command => "sudo service nginx restart",
         provider => 'shell',
 }
