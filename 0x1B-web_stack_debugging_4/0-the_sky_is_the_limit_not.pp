0652d3ccc02d73fd3eb2f438294dc870b96cd1c1
juansu01 <thejuank0@hotmail.com>
2021-10-07 12:48:01 -0500

    Increasing workers to 100 and adding a new comment

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # Double the amount of worker processes for nginx
 exec { "change workers":
-        command => "sed -i 's/worker_processes 4;/worker_processes 16;/g' /etc/nginx/nginx.conf",
+        command => "sed -i 's/worker_processes 4;/worker_processes 100;/g' /etc/nginx/nginx.conf",
         provider=> 'shell',
 }
-
+# Restart the nginx service
 exec { "restart service":
         command => "sudo service nginx restart",
         provider => 'shell',
