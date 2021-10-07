55af2ef502e13c23b5108fc35a9c11594ad132b6
Juan Camilo <70981507+Juansu01@users.noreply.github.com>
2021-10-07 13:11:53 -0500

    fixed syntax

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,10 @@
 # Double the amount of worker processes for nginx
 exec { "change workers":
         command => "sed -i 's/worker_processes 4;/worker_processes 100;/g' /etc/nginx/nginx.conf",
+        provider => 'shell',
+}
+# Restart the nginx service
+exec { "restart service":
         command => "sed -i 's/# multi_accept on;/multi_accept on;/g' /etc/nginx/nginx.conf",
         provider => 'shell',
 }
