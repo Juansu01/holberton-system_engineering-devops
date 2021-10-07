0609cfcbce90264048d2d6908e389b5df02f00d9
Juan Camilo <70981507+Juansu01@users.noreply.github.com>
2021-10-07 13:38:32 -0500

    trying simpler solution

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,4 @@
-# Double the amount of worker processes for nginx
-exec { "change workers":
-        command => "sed -i 's/worker_processes 4;/worker_processes 100;/g' /etc/nginx/nginx.conf",
-        provider => 'shell',
-}
+# Increment amount of open files
 exec { "increment open files":
         command => "sed -i s/15/2000/ /etc/default/nginx",
         provider => 'shell',
