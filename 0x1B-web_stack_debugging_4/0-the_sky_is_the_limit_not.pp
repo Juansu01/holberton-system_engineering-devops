e02bf7dca19082e2a5b5ee001213856435dcf978
juansu01 <thejuank0@hotmail.com>
2021-10-08 07:00:14 -0500

    Increasing workers

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,8 @@
 # Increment amount of open files
+exec { 'change workers':
+        command  => "sed -i 's/worker_processes 4;/worker_processes 100;/g' /etc/nginx/nginx.conf",
+        provider => 'shell',
+}
 exec { 'increment open files':
         command  => 'sed -i s/15/2000/ /etc/default/nginx',
         provider => 'shell',
