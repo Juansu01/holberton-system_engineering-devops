a89ca7727b5007d447605b346871e29c5fad70c4
juansu01 <thejuank0@hotmail.com>
2021-10-08 06:55:49 -0500

    Fixing code style

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # Increment amount of open files
-exec { "increment open files":
-        command => "sed -i s/15/2000/ /etc/default/nginx",
+exec { 'increment open files':
+        command  => 'sed -i s/15/2000/ /etc/default/nginx',
         provider => 'shell',
 }
-exec { "restart service":
-        command => "sudo service nginx restart",
+exec { 'restart service':
+        command  => 'sudo service nginx restart',
         provider => 'shell',
 }
